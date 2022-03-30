32840beb914b0b8b7e04f8816cbc5d0c85a06866
Tenshi <tensitune@gmail.com>
2022-03-30 16:20:51 +0500

    update ecosystem.config.js and package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,20 @@
 module.exports = {
   apps: [
     {
+      name: "node-fades-discord",
       script: "./dist/index.js",
-      name: "fades-discord",
-      time: true,
+
+      // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "512M",
+      env: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
     },
   ],
 };
